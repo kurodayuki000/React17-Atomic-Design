bbfdc07fecbe36b2625671960f948f7dbc78eea8
kurodayuuki <kurodayuki07122170@gmail.com>
2022-06-23 17:43:29 +0900

    atmic Dsign完

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Top = () => {
+  return(
+    <SContaicner>
+      <h2>TOPページです</h2>
+    </SContaicner>
+  )
+}
+
+const SContaicner = styled.div`
+  text-align: center;
+`
---
+++ src/components/templates/HeaderOnly.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../atoms/layout/Header";
+import styled from "styled-components";
+
+export const HeaderOnly = (props) => {
+  const {children} = props;
+  return(
+    <>
+      <Header>
+
+      </Header>
+      {children}
+    </>
+  )
+}
